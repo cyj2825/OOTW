4b92f0b3b95878cb3ce6532f81384c4e9b4ec0dd
juhee-dev <viimv2000@gmail.com>
2022-04-18 22:19:11 +0900

    add fragment_bookmark.xml

--- app/src/main/java/com/example/ootw/fragment/BookmarkFragment.kt
+++ app/src/main/java/com/example/ootw/fragment/BookmarkFragment.kt
@@ -14,7 +14,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BookmarkFragment.newInstance] factory method to
+ * Use the [ClosetFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class BookmarkFragment : Fragment() {
@@ -45,12 +45,12 @@ class BookmarkFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BookmarkFragment.
+         * @return A new instance of fragment ClosetFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BookmarkFragment().apply {
+            ClosetFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
