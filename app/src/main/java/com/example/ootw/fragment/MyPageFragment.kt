bbcde2d576129c8fbb82afd13be94c33c88a7fa4
juhee-dev <viimv2000@gmail.com>
2022-04-24 05:53:01 +0900

    add NoticeFragment.kt

--- app/src/main/java/com/example/ootw/fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/ootw/fragment/MyPageFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import com.example.ootw.R
 import com.example.ootw.databinding.FragmentMyPageBinding
@@ -13,7 +12,7 @@ import com.example.ootw.databinding.FragmentMyPageBinding
 class MyPageFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentMyPageBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,20 +23,29 @@ class MyPageFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_my_page, container, false)
-        val btn_notice: TextView = view.findViewById(R.id.tv_my_page_notice)
-        btn_notice.setOnClickListener(this)
+        view.findViewById<TextView>(R.id.tv_my_page_like).setOnClickListener(this)
+        view.findViewById<TextView>(R.id.tv_my_page_notice).setOnClickListener(this)
         return view
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
+            R.id.tv_my_page_update -> {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_screen_panel, NoticeFragment()).commitNow()
+            }
+            R.id.tv_my_page_like -> {
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.main_screen_panel, LikeFragment()).commitNow()
+            }
             R.id.tv_my_page_notice -> {
                 parentFragmentManager.beginTransaction()
-                    .replace(R.id.vp_ac_main_frag_pager, NoticeFragment()).commitNow()
+                    .replace(R.id.main_screen_panel, NoticeFragment()).commitNow()
             }
 
             else -> {
             }
         }
     }
+
 }
