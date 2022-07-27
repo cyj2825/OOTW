cea1f832a27ec110f9415cd2ffba66f913534541
juhee-dev <viimv2000@gmail.com>
2022-07-27 16:02:20 +0900

    LikeFragment.kt 수정(출력안됨)

--- app/src/main/java/com/example/ootw/fragment/LikeFragment.kt
+++ app/src/main/java/com/example/ootw/fragment/LikeFragment.kt
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.ootw.R
+import com.example.ootw.model.Post
 
 class LikeFragment : Fragment() {
     private lateinit var callback: OnBackPressedCallback
@@ -35,5 +41,37 @@ class LikeFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    inner class LikeFragmentRecyclerViewAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var postDTOs = arrayListOf<Post>(
+            // test 값
+            Post("test1", "", "오늘은 참 덥네요!", "", 26, "나시", "2022-07-20T07:20:07.000Z", "2022-07-20T07:20:07.000Z", "user1", ""),
+
+            )
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+
+            var imageview = ImageView(parent.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView): RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return postDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(postDTOs[position].imgURL).apply(
+                RequestOptions().centerCrop()).into(imageview)
+            Log.d("LikeFragment", "post: "+postDTOs[position].id)
+
+        }
     }
 }
