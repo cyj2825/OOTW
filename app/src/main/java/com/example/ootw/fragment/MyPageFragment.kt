3fbd1497c277f0abc142eb9c5cf6873a8315f5f3
juhee-dev <viimv2000@gmail.com>
2022-07-28 17:18:37 +0900

    로그아웃 기능 마이페이지로 이동, 화면 디자인 수정

--- app/src/main/java/com/example/ootw/fragment/MyPageFragment.kt
+++ app/src/main/java/com/example/ootw/fragment/MyPageFragment.kt
@@ -1,12 +1,15 @@
 package com.example.ootw.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.commit
+import com.example.ootw.LoginActivity
 import com.example.ootw.R
 import com.example.ootw.databinding.FragmentMyPageBinding
 
@@ -23,10 +26,11 @@ class MyPageFragment : Fragment(), View.OnClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_my_page, container, false)
-        view.findViewById<TextView>(R.id.tv_my_page_update).setOnClickListener(this)
-        view.findViewById<TextView>(R.id.tv_my_page_notice).setOnClickListener(this)
-        return view
+        binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        binding.tvMyPageUpdate.setOnClickListener(this)
+        binding.tvMyPageNotice.setOnClickListener(this)
+        binding.tvMyPageLogout.setOnClickListener(this)
+        return binding.root
     }
 
     override fun onClick(v: View?) {
@@ -38,6 +42,12 @@ class MyPageFragment : Fragment(), View.OnClickListener {
             R.id.tv_my_page_notice -> {
                 parentFragmentManager.beginTransaction()
                     .replace(R.id.main_screen_panel, NoticeFragment()).commitNow()
+            }
+            R.id.tv_my_page_logout -> {
+                val intent = Intent(requireContext(), LoginActivity::class.java)
+                startActivity(intent)
+                requireActivity().overridePendingTransition(0, 0)
+                Toast.makeText(requireContext(), "로그아웃 완료!!", Toast.LENGTH_SHORT).show()
             }
 
             else -> {
