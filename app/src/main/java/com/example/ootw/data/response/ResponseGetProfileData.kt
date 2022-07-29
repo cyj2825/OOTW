c7c65cd792c9aa978573efd075f28966a609dc26
juhee-dev <viimv2000@gmail.com>
2022-07-29 13:10:26 +0900

    디자인 수정

--- app/src/main/java/com/example/ootw/data/response/ResponseGetProfileData.kt
+++ app/src/main/java/com/example/ootw/data/response/ResponseGetProfileData.kt
@@ -2,8 +2,19 @@ package com.example.ootw.data.response
 
 import com.example.ootw.model.Post
 import com.example.ootw.model.User
+import com.kakao.sdk.user.model.Gender
 
 data class ResponseGetProfileData(
     val message: String,
-    val userProfile: User,
+    val userProfile: UserProfileData,
 )
+
+data class UserProfileData (
+    val nickname: String,
+    val gender: String,
+    val birth: String,
+    val area: String,
+    val area_detail: String,
+    val cold_sensitivity: Int,
+    val hot_sensitivity: Int
+    )
